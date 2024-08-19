9c795cb5872779e40a736542cd62a5ad51b1bb18
amyj0hn <amyj62450@gmail.com>
2024-08-19 15:53:12 +0200

    CreatePoolConnection

--- config/index.js
+++ config/index.js
@@ -1,1 +1,20 @@
-// 
+import { createPool } from "mysql2";
+import "dotenv/config"
+
+let connection = createPool({
+    host: process.env.hostdb,
+    database: process.env.databaseName,
+    user: process.env.userdb,
+    password: process.env.passworddb,
+    multipleStatements: true,
+    connectionLimit: 50
+})
+
+connection.on('connection', (pool) => {
+    if(!pool) throw new Error('Unable to Connect')
+})
+
+// Always EXPORT
+export{
+    connection
+}
