2b4cd1046215b31a05c7858aeb3716be724402a0
MasoodahGeorge <oodasbusiness@gmail.com>
2024-08-19 16:12:21 +0200

    first commit

---
+++ src/components/NavComp.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid justify-content-center">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav text-center">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/products">Products</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/admin">Admin</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/contact">Contact</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavbarComp'
+  }
+  </script>
+  
+  <style scoped>
+  .navbar-nav .nav-link {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+  }
+  .navbar-nav .nav-link:hover {
+    color: #42b983;
+  }
+  </style>
+  
