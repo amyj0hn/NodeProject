31078691c5577475a6f005d54cae3c2d6e4b2fda
amyj0hn <amyj62450@gmail.com>
2024-08-20 08:51:06 +0200

    middleware, routers and start server

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-import { express } from "./controller/userController.js"
-
+import { userRouter, express } from "./controller/userController.js"
+import {productRouter} from "./controller/productController.js"
+import path from "path";
 // Create an express app
 const app = express();
 const port = +process.env.PORT || 4001;
 
 // Middleware
+app.use(
+    express.static("./static"),
+    express.json(),
+    express.urlencoded({
+        extended:true
+    })
+)
 
 // Routers
-
+app.use('/Users', userRouter)
+app.use('/Products', productRouter)
 // Endpoint
+app.get("^/$|/NodeProject", (req, res => {
+    res.status(200).sendFile(path.resolve("./static/html/index.html"));
+}))
 
 // Start the server
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
