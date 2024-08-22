f9ad37bff7ba36a133be19c065e678699396b39d
amyj0hn <amyj62450@gmail.com>
2024-08-22 15:12:54 +0200

    removed the s from endpoints

--- model/index.js
+++ model/index.js
@@ -1,5 +1,5 @@
-import {Users} from "./users.js";
-import {Products} from "./products.js";
+import { Users } from "./users.js";
+import { Products } from "./products.js";
 
 
 const users = new Users()
