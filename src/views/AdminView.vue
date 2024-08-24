0b3fed2a518576f6a23ae97244c6a94a64543a55
MasoodahGeorge <oodasbusiness@gmail.com>
2024-08-24 09:30:26 +0200

    added usrs table

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -55,7 +55,7 @@
                     >Description</label
                   >
                   <input
-                    v-model="currentProduct.amount"
+                    v-model="newProduct.amount"
                     type="text"
                     class="form-control"
                     id="editDescription"
@@ -99,50 +99,6 @@
           </div>
         </div>
       </div>
-<<<<<<< HEAD
-=======
-    </main>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import { useStore } from 'vuex';
-  
-  const store = useStore();
-  const products = ref([]);
-  const newProduct = ref({
-    prodName: '',
-    Category: '',
-    amount: 0,
-    prodUrl: ''
-  });
-  const currentProduct = ref({});
-  
-  const fetchProducts = async () => {
-    await store.dispatch('fetchProducts');
-    products.value = store.state.products;
-  };
-  
-  const handleAddProduct = async () => {
-    await store.dispatch('addAProduct', newProduct.value);
-    products.value = store.state.products;
-    newProduct.value = { prodName: '', Category: '', amount: 0, prodUrl: '' };
-  };
-  
-  const handleEditProduct = (product) => {
-    currentProduct.value = { ...product };
-  };
-  
-  // const handleUpdateProduct = async () => {
-  //   await store.dispatch('updateProduct', currentProduct.value);
-  //   products.value = store.state.products;
-  // };
-  
-  const handleDeleteProduct = async (prodID) => {
-    await store.dispatch('deleteProduct', prodID);
-    products.value = store.state.products;
-  };
->>>>>>> 32746195b5e5158de350adfeb09398a2ef1ea07a
 
       <!-- Edit Product Modal -->
       <div
@@ -275,6 +231,183 @@
         </table>
       </div>
     </div>
+
+    <!-- Add User Button -->
+    <div>
+        <button
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#addUserModal"
+        >
+          <i class="bi bi-plus-circle-fill"></i> Add User
+        </button>
+      </div>
+
+      <!-- Add User Modal -->
+      <div
+        class="modal fade"
+        id="addUserModal"
+        tabindex="-1"
+        aria-labelledby="addUserModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="addUserModalLabel">
+                Add New User
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <!-- Form to add new user -->
+              <form @submit.prevent="handleAddUser">
+                <div class="mb-3">
+                  <label for="username" class="form-label">Username</label>
+                  <input
+                    v-model="newUser.username"
+                    type="text"
+                    class="form-control"
+                    id="username"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input
+                    v-model="newUser.email"
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input
+                    v-model="newUser.password"
+                    type="password"
+                    class="form-control"
+                    id="password"
+                    required
+                  />
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Add User
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Edit User Modal -->
+      <div
+        class="modal fade"
+        id="editUserModal"
+        tabindex="-1"
+        aria-labelledby="editUserModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editUserModalLabel">
+                Edit User
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <!-- Form to edit user -->
+              <form @submit.prevent="handleEditUser">
+                <div class="mb-3">
+                  <label for="editUsername" class="form-label">Username</label>
+                  <input
+                    v-model="currentUser.username"
+                    type="text"
+                    class="form-control"
+                    id="editUsername"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="editEmail" class="form-label">Email</label>
+                  <input
+                    v-model="currentUser.email"
+                    type="email"
+                    class="form-control"
+                    id="editEmail"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="editPassword" class="form-label">Password</label>
+                  <input
+                    v-model="currentUser.password"
+                    type="password"
+                    class="form-control"
+                    id="editPassword"
+                  />
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Save Changes
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Users Table -->
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(user, index) in users" :key="user.id">
+              <td>{{ index + 1 }}</td>
+              <td>{{ user.username }}</td>
+              <td>{{ user.email }}</td>
+              <td>
+                <button
+                  class="btn btn-warning"
+                  mt-2
+                  @click="handleEditUser(user)"
+                  data-bs-toggle="modal"
+                  data-bs-target="#editUserModal"
+                >
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button
+                  class="btn btn-danger"
+                  @click="handleDeleteUser(user.id)"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
   </main>
 </template>
 
@@ -282,6 +415,7 @@
 import { ref, onMounted } from "vue";
 import { useStore } from "vuex";
 
+// Products
 const store = useStore();
 const products = ref([]);
 const newProduct = ref({
@@ -309,17 +443,65 @@ const handleEditProduct = async (product) => {
   Object.assign(currentProduct.value, product);
 };
 
-// const handleUpdateProduct = async () => {
-//   await store.dispatch('updateProduct', currentProduct.value);
-//   products.value = store.state.products;
-// };
-
 const handleDeleteProduct = async (prodID) => {
   await store.dispatch("deleteProduct", prodID);
   products.value = store.state.products;
 };
 
 onMounted(fetchProducts);
+
+// Users
+import axios from "axios";
+
+const apiUrl = "https://nodeproject-0wx0.onrender.com/";
+
+const users = ref([]);
+const newUser = ref({
+  username: "",
+  email: "",
+  password: "",
+});
+const currentUser = ref({});
+
+const fetchUsers = async () => {
+  try {
+    const response = await axios.get(`${apiUrl}users`);
+    users.value = response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+};
+
+const handleAddUser = async () => {
+  try {
+    await axios.post(`${apiUrl}users`, newUser.value);
+    await fetchUsers();
+    newUser.value = { username: "", email: "", password: "" };
+  } catch (error) {
+    console.error("Error adding user:", error);
+  }
+};
+
+const handleEditUser = async () => {
+  try {
+    await axios.put(`${apiUrl}users/${currentUser.value.id}`, currentUser.value);
+    await fetchUsers();
+  } catch (error) {
+    console.error("Error editing user:", error);
+  }
+};
+
+const handleDeleteUser = async (userId) => {
+  try {
+    await axios.delete(`${apiUrl}users/${userId}`);
+    await fetchUsers();
+  } catch (error) {
+    console.error("Error deleting user:", error);
+  }
+};
+
+onMounted(fetchUsers);
+
 </script>
 
 <style scoped>
