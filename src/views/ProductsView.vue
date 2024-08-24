98f0ab52866d406fb3da3c088901f4dfa507a391
amyj0hn <amyj62450@gmail.com>
2024-08-24 10:26:24 +0200

    Responsiveness part 2

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,37 +1,4 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <h2 class="display-2">Products</h2>
-        </div>
-        <div class="row gap-2 justify-content-center my-2" id="productgrid" v-if="products">
-            <Card v-for="product in products" :key="product.prodID" class="card">
-                <template #cardHeader>
-                    <img :src="product.prodUrl" loading="lazy" class="img-fluid" :alt="product.prodName">
-                </template>
-                <template #cardBody>
-                    <h5 class="card-title fw-bold">
-                        {{ product.prodName }}
-                    </h5>
-                    <p class="lead">
-                        <span class="text-success fw-bold">
-                            Amount
-                        </span>: R{{ product.amount }}</p>
-                        <p class="card-text" v-if="product.prodDescription">{{ product.prodDescription }}</p>
-                        <p class="card-text" v-else>No description available.</p>
-                    <div class="button-wrapper d-md-flex d-block justify-content-between">
-                        <router-link :to="`/product/${product.prodID}`">
-                            <button class="btn btn-success">
-                                View
-                            </button>
-                        </router-link>
-                    </div>
-                </template>
-            </Card>
-        </div>
-        <div v-else>
-            <Spinner />
-        </div>
-    </div>
   <div class="container">
     <div class="row">
       <h2 class="display-2">Products</h2>
